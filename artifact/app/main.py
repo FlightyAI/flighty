74701e76b9b8c12981eb5ba2d4cd84e3749b3ff3
Gopal K. Vashishtha <gopalvashishtha@college.harvard.edu>
2022-07-12 09:59:06 -0500

    artifacts works locally, needs deployment now

--- artifact/app/main.py
+++ artifact/app/main.py
@@ -1,8 +1,12 @@
+from enum import Enum
 from fastapi import FastAPI, HTTPException, UploadFile
 from pydantic import BaseModel
 
 import mysql.connector
 import os
+import shutil
+import uvicorn
+
 
 # TODO - factor this out into shared library across all services
 try:   
@@ -14,35 +18,68 @@ except mysql.connector.errors.DatabaseError:
     except mysql.connector.errors.DatabaseError:
         # if we're running in Docker
         cnx = mysql.connector.connect(host="host.docker.internal", password="password", database="flighty")
+
 c=cnx.cursor(dictionary=True)
 
 app = FastAPI()
 
+class ArtifactTypeEnum(str, Enum):
+    code = 'code'
+    model = 'model'
+
 class Artifact(BaseModel):
     name: str
     version: int = 0
-    file: UploadFile
-    type: str
+    path: str
+    type: ArtifactTypeEnum
 
 # Write file to subpath of mounted volume
-# Then store this path inside of database
+# Then store the full path inside of the database
 # We'll then mount that volume to containers we spin up
 @app.post("/create")
-async def create_artifact(file: UploadFile, name: str, version: int, type: str):
-    dir_path = f'./flighty-files/{name}/{version}'
+async def create_artifact(file: UploadFile, name: str, version: int, type: ArtifactTypeEnum):
+    db_path = os.path.join('flighty-files', name, str(version))
+    dir_path = os.path.join(os.getcwd(), db_path)
     os.makedirs(dir_path, exist_ok=True)
-    with open(f'{dir_path}/{file.filename}', 'wb') as f:
+
+    path = os.path.join(dir_path, file.filename)
+    with open(path, 'wb') as f:
         f.write(file.file.read())
+    
+    try:
+        shutil.unpack_archive(path, dir_path) # unpack zip file if it is a zip
+        os.remove(path)
+    except shutil.ReadError as e: # not a zip file
+        pass
+
+    # TODO - There is a dependency between where artifact is writing files and where handler expects to read them
+    # Need to figure out a way to factor that dependency into one common place
+    artifact = Artifact(name=name, version=version, path='/' + db_path, type=type)
+
+    try:
+        c.execute(
+                """INSERT INTO artifacts
+                (name, path, version, type) VALUES (%s, %s, %s, %s)
+                """, (artifact.name, artifact.path, artifact.version, 
+                    artifact.type)
+        )
+        cnx.commit()
+    except mysql.connector.errors.IntegrityError:
+        c.execute("SELECT MAX(version) AS max FROM artifacts WHERE name = %s", (artifact.name,))
+        last_version = c.fetchone()['max']
+        raise HTTPException(status_code=422, detail=f"""Artifact {name} with version {version} already exists. 
+            Specify a version larger than {last_version} and try again.""")
+    return artifact
 
 
+@app.get("/list")
+async def list_artifacts(name: str = None):
+    if name:
+        c.execute("SELECT * FROM artifacts WHERE name = %s", (name,))
+        return c.fetchall()
+    else:
+        c.execute("SELECT * FROM artifacts")
+        return c.fetchall()
 
-
-@app.post("/create_handler")
-def create_pod():
-  containers = [client.V1Container(name='handler', image='docker.io/gvashishtha/flighty:handler')]
-  spec = client.V1PodSpec(
-      containers=containers, 
-      image_pull_secrets=[client.V1LocalObjectReference(name='regcred')])
-  metadata=client.V1ObjectMeta(name='test1', labels={"flighty": "mlservice-cpu"})
-  pod = client.V1Pod(metadata=metadata, spec=spec)
-  v1.create_namespaced_pod(namespace='default', body=pod)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
