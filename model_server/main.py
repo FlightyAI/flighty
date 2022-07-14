cf3dab350b104232f8f0d22fed8c7bdf4edceadd
Gopal K. Vashishtha <gopalvashishtha@college.harvard.edu>
2022-07-13 23:32:21 -0500

    could not find better solution for inferring types, need to now run this thing in k8s then as helm chart

--- model_server/main.py
+++ model_server/main.py
@@ -1,21 +1,22 @@
 from typing import Union
 from customer_code import main
 from fastapi import Depends, FastAPI
-from inspect import signature
+from inspect import signature, Parameter
 from pydantic import BaseModel, create_model
 
 import json
 import uvicorn
 
 app = FastAPI()
-
+# Get the types of the user's predict function
 sig = signature(main.predict)
 
 query_params = {}
 for k in sig.parameters:
   query_params[k] = (sig.parameters[k].annotation, ...)
 
-query_model = create_model("Query", **query_params) # This is subclass of pydantic BaseModel
+query_model = create_model("Query", **query_params)
+
 
 @app.on_event("startup")
 async def startup_event():
@@ -26,7 +27,7 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/infer")
-def predict(params: query_model = Depends()):
+def predict(params: query_model): # = Depends()):
   p_as_dict = params.dict()
   return main.predict(**p_as_dict)
 
